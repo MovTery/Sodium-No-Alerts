9dc7c39c86ec524fcd7e9789e25b77ff382b3f8f
movte <212824502@qq.com>
2024-08-11 12:27:10 +0800

    1.20.1

--- src/main/java/com/movtery/mixin/PostLaunchChecksMixin.java
+++ src/main/java/com/movtery/mixin/PostLaunchChecksMixin.java
@@ -1,13 +1,13 @@
 package com.movtery.mixin;
 
 import com.movtery.SodiumNoAlerts;
-import me.jellysquid.mods.sodium.client.compatibility.checks.LateDriverScanner;
+import me.jellysquid.mods.sodium.client.compatibility.checks.PostLaunchChecks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LateDriverScanner.class)
+@Mixin(PostLaunchChecks.class)
 public class PostLaunchChecksMixin {
     @Inject(method = "isUsingPojavLauncher", at = @At("RETURN"), cancellable = true)
     private static void isUsingPojavLauncher(CallbackInfoReturnable<Boolean> cir) {
